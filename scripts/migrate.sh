069cc3fe2438f9b4c8583752dd649ef3bb2026a4
kirinnee <kirinnee97@gmail.com>
2024-09-26 13:34:13 +0800

    feat: allow for scheduling of backups

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,7 +4,23 @@ set -eou pipefail
 
 from="$1"
 to="$2"
-backup_name="$3"
+
+backups="$(kubectl get backup --context "${to}" -n velero -o json | jq -r '.items[].metadata.name')"
+
+count="$(echo "${backups}" | wc -l)"
+
+backup_name="m0001"
+
+if [ "$count" -gt 0 ]; then
+  latest="$(echo "${backups}" | sort | tail -n 1)"
+  echo "✨ Latest: ${latest}"
+  current=${latest//[!0-9]/}
+  echo "🔢 Current: ${current}"
+  incremented=$((10#$current + 1))
+  echo "🔢 Incremented: ${incremented}"
+  backup_name=$(printf "m%04d" $incremented)
+  echo "🔢 Backup name: ${backup_name}"
+fi
 
 velero --kubecontext "${from}" backup create --include-namespaces "sulfoxide-helium,sulfoxide-boron" "${backup_name}" -w
 
